5d79b5791b18ec4d4c50c1c080c68e0e5bd000c3
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-03-10 22:55:26 +0500

    Added basic Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './components/recipeBook/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shoppingList/shopping-list/shopping-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/recipes', pathMatch: 'full'},
+    {path:'recipes', component: RecipesComponent},
+    {path:'shoppinglist',component: ShoppingListComponent}
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DropdownDirective } from '../../shared/dropdown.directive';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [DropdownDirective],
+  imports: [DropdownDirective, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  @Output() recipesClicked= new EventEmitter<string>();
-  @Output() shoppingListClicked= new EventEmitter<string>();
-  onSelect(feature: string){
-    if(feature === 'Shopping List')
-      this.shoppingListClicked.emit(feature);
-    else if(feature === 'Recipes')
-    this.recipesClicked.emit(feature);
-  }
 
 }
