0cb6aa070bb4c2aff7189b20ad2836c857c6663f
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-03-15 18:41:03 +0500

    Add Shopping List Add Ingredient Functionality using FormsModule

--- src/app/components/shoppingList/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shoppingList/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,24 +1,24 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../../../shared/ingredient.modal';
 import { ShoppingListService } from '../shopping-list.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './shopping-list-edit.component.html',
   styleUrl: './shopping-list-edit.component.css'
 })
 export class ShoppingListEditComponent {
-  @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
 
   constructor(private shoppingListService:ShoppingListService){
 
   }
 
-  onAddItem() {
-    const newingredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value)
+  onAddItem(form:NgForm) {
+    const value = form.value;
+    const newingredient = new Ingredient(value.name,value.amount)
     this.shoppingListService.addIngredient(newingredient);
   }
 }
